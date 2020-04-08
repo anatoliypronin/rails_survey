91629d62d9a17826337aa39244e3e3e9259d1f68
Petr Ablyazov <phoenix-h@mail.ru>
2020-04-08 21:59:51 +0400

    Add new test on change state in Admin

--- test/models/admin_test.rb
+++ test/models/admin_test.rb
@@ -13,4 +13,10 @@ class AdminTest < ActiveSupport::TestCase
     admin = build :admin, email: email
     assert_not admin.save
   end
+
+  test "should transite state to archived" do
+    admin = create :admin
+    admin.archived
+    assert_equal  admin.state, 'archived'
+  end
 end
