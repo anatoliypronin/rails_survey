1eff6b2c5ec1380a87c2adffdb768d377bcd1022
Petr Ablyazov <phoenix-h@mail.ru>
2020-04-08 14:31:41 +0400

    Remark from rubocop

--- test/models/admin_test.rb
+++ test/models/admin_test.rb
@@ -2,15 +2,15 @@ require 'test_helper'
 
 class AdminTest < ActiveSupport::TestCase
   test "should create admin" do
-    admin = create :admin
+    create :admin
     created_admin = Admin.last
     assert created_admin
   end
 
   test "should email unic" do
     email = 'unic@test.ru'
-    admin = create :admin, email: email
-    admin2 = build :admin, email: email
-    assert_not admin2.save
+    create :admin, email: email
+    admin = build :admin, email: email
+    assert_not admin.save
   end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,8 +3,6 @@ require_relative '../config/environment'
 require 'rails/test_help'
 
 class ActiveSupport::TestCase
-  # Run tests in parallel with specified workers
   include FactoryBot::Syntax::Methods
   parallelize(workers: :number_of_processors)
-
 end
