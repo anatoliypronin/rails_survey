481f32575161f8e8ee57c5a2cc7561da06750001
Petr Ablyazov <phoenix-h@mail.ru>
2020-04-14 10:21:34 +0400

    Add method show

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -2,4 +2,15 @@ class Admin::UsersController < Admin::ApplicationController
   def index
     @users = User.all
   end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  private
+
+  def user_attrs
+    params.require(:user).permit(:first_name, :last_name, :email, :phone, :password)
+  end
+  
 end
--- test/controllers/admin/admin_controller_test.rb
+++ test/controllers/admin/admin_controller_test.rb
@@ -7,4 +7,18 @@ class Admin::UsersControllerTest < ActionDispatch::IntegrationTest
     get admin_users_path
     assert_response :success
   end
+
+  test 'should get show admin' do
+    admin = create :admin
+
+    get admin_user_path(admin)
+    assert_response :success
+  end
+
+  test 'should get show respondent' do
+    respondent = create :respondent
+
+    get admin_user_path(respondent)
+    assert_response :success
+  end
 end
