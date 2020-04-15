e50435a9fa8932b3afce494a6d0ee7fe0ea832f4
Petr Ablyazov <phoenix-h@mail.ru>
2020-04-15 11:19:31 +0400

    Add create method for Respondent

--- test/controllers/admin/respondent_controller_test.rb
+++ test/controllers/admin/respondent_controller_test.rb
@@ -5,4 +5,14 @@ class Admin::RespondentsControllerTest < ActionDispatch::IntegrationTest
     get new_admin_respondent_path
     assert_response :success
   end
+
+  test 'should post create respondent' do
+    attrs = attributes_for(:respondent)
+
+    post admin_respondents_path, params: { respondent: attrs }
+    assert_response :redirect
+
+    respondent = Respondent.last
+    assert_equal respondent.phone, attrs[:phone]
+  end
 end
