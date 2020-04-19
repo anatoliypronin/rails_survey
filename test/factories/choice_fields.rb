d972dc797e78a336f78bffa4cd2e48b39cb2c9a1
sanyk15 <sanekmakienko322@gmail.com>
2020-04-19 20:39:57 +0400

    Remove type

--- test/factories/choice_fields.rb
+++ test/factories/choice_fields.rb
@@ -1,6 +1,5 @@
 FactoryBot.define do
   factory :choice_field do
     question
-    type
   end
 end
--- test/factories/other_fields.rb
+++ test/factories/other_fields.rb
@@ -1,7 +1,6 @@
 FactoryBot.define do
   factory :other_field do
     question
-    type
     title
   end
 end
