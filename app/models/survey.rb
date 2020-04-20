afe5403779b71e913651b14e4d4699874b878e55
sanyk15 <sanekmakienko322@gmail.com>
2020-04-21 01:02:32 +0400

    rubocop fix

--- app/models/survey.rb
+++ app/models/survey.rb
@@ -2,7 +2,7 @@ class Survey < ApplicationRecord
   has_many :tags, through: :tag_surveys
   has_many :tag_surveys, dependent: :destroy
   has_many :questions, dependent: :destroy
-  #belongs_to :user
+  # belongs_to :user
   validates :title, presence: true
   state_machine initial: :active do
     state :active
