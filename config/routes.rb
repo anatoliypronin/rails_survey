509c5ed1ff8bc103377205093fa0483c299bd03d
sanyk15 <sanekmakienko322@gmail.com>
2020-04-21 15:20:47 +0400

    fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   namespace :admin do
     resources :users, only: %i[index show edit update] do
       member do
-        put "in_archive"
-        put "restore"
+        put :in_archive
+        put :restore
       end
     end
     resources :admins, only: %i[new create]
