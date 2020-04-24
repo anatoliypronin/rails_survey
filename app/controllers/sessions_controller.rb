4555792661942fa3dd96991ca057e95aae87781c
Petr Ablyazov <phoenix-h@mail.ru>
2020-04-24 11:49:06 +0400

    Add method create for Session

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,6 +3,15 @@ class SessionsController < ApplicationController
   end
   
   def create
+    admin = Admin.find_by(email: params[:admin][:email])
+
+    if admin && admin.authenticate(params[:admin][:password])
+      session[:admin_id] = admin.id
+      redirect_to admin_root_path
+    else
+      render action: :new
+    end
+
   end
 
   def destroy
--- test/controllers/sessions_controller_test.rb
+++ test/controllers/sessions_controller_test.rb
@@ -5,4 +5,20 @@ class SessionsControllerTest < ActionDispatch::IntegrationTest
     get new_session_path
     assert_response :success
   end
+
+  test 'should post create session page for admin' do
+    admin = create :admin
+    post session_path, params: {admin: { email: admin.email, password: admin.password }}
+    assert_response :redirect
+
+    assert_equal session[:admin_id], admin.id
+  end
+
+  test 'should not post create session page for admin' do
+    admin = create :admin
+    post session_path, params: {admin: { email: admin.email, password: 'bad_password'}}
+    assert_response :success
+
+    assert_nil session[:admin_id]
+  end
 end
