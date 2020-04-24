4c26311c71c1ccccd4c720798a1dd8e77512f100
Petr Ablyazov <phoenix-h@mail.ru>
2020-04-24 15:44:04 +0400

    Add auth in test_helper. Change all tests Admin with authenticate

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,7 +2,23 @@ ENV["RAILS_ENV"] ||= "test"
 require_relative "../config/environment"
 require "rails/test_help"
 
+module SignInHelper
+  def sign_in_as_admin(admin)
+    post session_path, params: {
+      user: {
+        password: admin.password,
+        email: admin.email
+      }
+    }
+  end
+end
+
 class ActiveSupport::TestCase
   include FactoryBot::Syntax::Methods
+  include AuthHelper
   parallelize(workers: :number_of_processors)
+end
+
+class ActionDispatch::IntegrationTest
+  include SignInHelper
 end
