6e9c781ada5caab5b253aab3574d4b4288ee33c1
Antonida <antonida619@gmail.com>
2020-04-27 23:15:13 +0400

    Rubocop correction and update model

--- app/models/tag_survey.rb
+++ app/models/tag_survey.rb
@@ -1,5 +1,5 @@
 class TagSurvey < ApplicationRecord
   belongs_to :survey
   belongs_to :tag
-  validates :article, uniqueness: { scope: :tag }
+  validates :title, uniqueness: { scope: :tag }
 end
--- db/migrate/20200426151134_create_survey_users.rb
+++ db/migrate/20200426151134_create_survey_users.rb
@@ -3,8 +3,8 @@ class CreateSurveyUsers < ActiveRecord::Migration[6.0]
     create_table :survey_users do |t|
       t.references :user_survey, null: false, foreign_key: true
       t.references :survey, null: false, foreign_key: true
-
       t.timestamps
     end
+  add_index :user_tags, %i[user_survey_id survey_id], unique: true
   end
 end
