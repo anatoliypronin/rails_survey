d40dbe078051a338873e428a1be83757d88b4bb2
Petr Ablyazov <phoenix-h@mail.ru>
2020-05-03 17:59:17 +0400

    Review

--- app/observers/admin_observer.rb
+++ app/observers/admin_observer.rb
@@ -1,7 +1,7 @@
 class AdminObserver < ActiveRecord::Observer
   observe :admin
   
-  def after_save(record)
+  def after_commit(record)
     AdminMailer.welcome_email(record).deliver_now
   end
 end
