9310ca52982b5a572f2a65c08969bdd918365c7c
Nailok <lexitor34@gmail.com>
2020-05-09 01:03:37 +0300

    Reworked destroy and create actions

--- app/controllers/admin/questions_controller.rb
+++ app/controllers/admin/questions_controller.rb
@@ -5,7 +5,6 @@ class Admin::QuestionsController < Admin::ApplicationController
 
   def new
     @question = Question.new
-    @survey = Survey.find(params[:survey_id])
   end
 
   def edit
@@ -18,29 +17,24 @@ class Admin::QuestionsController < Admin::ApplicationController
 
   def create
     @question = Question.new(question_attrs)
-    @survey = Survey.find(params[:survey])
+    @survey = Survey.find(params[:survey_id])
 
     if @survey
       @question.survey = @survey
     end
 
     if @question.save
-      redirect_to admin_survey_questions_path(@survey.id)
+      redirect_to admin_survey_path(@survey)
     else
       render action: :new
     end
   end
 
   def destroy
-    question = Question.find(params[:id])
-    @survey = Survey.find(params[:survey])
+    @question = Question.find(params[:id])
+    @question.destroy
 
-    if @survey
-      @question.survey = @survey
-    end
-
-    question.destroy
-    redirect_to admin_survey_questions_path
+    redirect_to admin_survey_path(@question.survey_id)
   end
 
   private
