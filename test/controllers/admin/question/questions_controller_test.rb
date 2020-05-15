ce5e71a92c7873052863d206cb820a7dc7331486
Nailok <lexitor34@gmail.com>
2020-05-15 13:06:12 +0300

    Add tests for questions page

---
+++ test/controllers/admin/question/questions_controller_test.rb
@@ -0,0 +1,15 @@
+require "test_helper"
+
+class Admin::Question::QuestionsControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    @question = create :question
+    admin = create :admin
+    sign_in_as_admin(admin)
+  end
+
+  test "should get index questions" do
+    create :tag
+    get admin_questions_path
+    assert_response :success
+  end
+end
