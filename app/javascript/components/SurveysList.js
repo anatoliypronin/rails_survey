31c4411e260c1b823eb12d7ddf33f1ba73888bd5
Petr Ablyazov <phoenix-h@mail.ru>
2020-05-19 15:37:00 +0400

    Add Component SurveysList

--- app/javascript/components/SurveysList.js
+++ app/javascript/components/SurveysList.js
@@ -1,11 +1,64 @@
 import React from "react"
 import PropTypes from "prop-types"
 class SurveysList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      surveys: [],
+    }
+  };
+  fetchSurvey() {
+    return fetch(Routes.api_v1_surveys_path({format: 'json'}))
+      .then(response => response.json())
+      .then(result => this.setState({surveys: result}))
+      
+  }
+  
+  componentDidMount() {
+    this.fetchSurvey();
+  }
+
+  getTags(tags){
+    let result = [];
+    for (let tag of tags) {
+      result.push(tag.title);
+
+    }
+    return (
+      result
+    );
+  }
+
+  test(){
+    let table = [];
+    const surveys = this.state.surveys;
+    for (let survey of surveys) {
+    table.push(<tr><td>{survey.title}</td><td>{this.getTags(survey.tags)}</td><a href={Routes.admin_survey_path(survey.id)}>Перейти</a></tr>);
+
+    }
+    return (
+      table
+    );
+  }
+
   render () {
     return (
       <React.Fragment>
-        <h1>Surveys</h1>
+      <h1>Surveys</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Tags</th>
+            <th>Control</th>
+          </tr>
+        </thead>
+        <tbody>
+          {this.test()}
+        </tbody>
+      </table>
       </React.Fragment>
+      
     );
   }
 }
