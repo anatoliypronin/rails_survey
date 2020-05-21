fe2e61d4c42ef7729904dbc6deb3c9270780ffd8
Petr Ablyazov <phoenix-h@mail.ru>
2020-05-21 16:58:54 +0400

    Add create method

--- app/controllers/web/respondent/answers_controller.rb
+++ app/controllers/web/respondent/answers_controller.rb
@@ -1,8 +1,17 @@
 class Web::Respondent::AnswersController < Web::Respondent::ApplicationController
   def new
-    @questions = Survey.find(params[:survey_id]).questions
+    @survey = Survey.find(params[:survey_id])
+    @questions = @survey.questions
   end
 
   def create
+    answers = params[:answers]
+    p answers
+    answers.each do |question_id, answer| 
+      q = Questiion.find(question_id)
+      
+      puts "#{question_id} is #{answer}" 
+    end
+    redirect_to root_path
   end
 end
--- test/controllers/web/respondent/answers_controller_test.rb
+++ test/controllers/web/respondent/answers_controller_test.rb
@@ -40,39 +40,12 @@ class Web::Respondent::AnswersControllerTest < ActionDispatch::IntegrationTest
     v6 = create :variant, question_id: q2.id
     v7 = create :variant, question_id: q2.id
 
-    p s1.questions
-    p ''
-    p q1
-    p ''
-    p q1.variants
-
-    p attributes_for :question
-
     answer_attrs = {q1.id => v1.id, q2.id => [v4.id, v6.id]}
-    p answer_attrs
+
     post respondent_survey_answers_path(s1), params: { answers: answer_attrs }
     assert_response :redirect
-
+    answer = Answer.last
+    assert_equal answer.survey_id, s1.id
   end
 
-  # test "should post create question" do
-  #   question_attrs = attributes_for :question
-  #   question_attrs[:survey_id] = @question.survey_id
-  #   post admin_survey_questions_path(@question.survey), params: { question: question_attrs }
-  #   assert_response :redirect
-
-  #   question = Question.last
-  #   assert_equal question_attrs[:title], question.title
-  # end
-  # test 'should delete destroy session page for respondent' do
-  #   respondent = create :respondent
-  #   sign_in_as_respondent(respondent)
-
-  #   assert_equal session[:user_id], respondent.id
-
-  #   delete respondent_session_path
-  #   assert_response :redirect
-
-  #   assert_nil session[:user_id]
-  # end
 end
