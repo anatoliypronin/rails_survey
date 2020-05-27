8831177a9af034939b4009c7fe19e0b56ada454b
Nailok <lexitor34@gmail.com>
2020-05-28 00:15:57 +0300

    Add profile editor for respondent

---
+++ app/controllers/web/respondent/profiles_controller.rb
@@ -0,0 +1,23 @@
+class Web::Respondent::ProfilesController < Web::Respondent::ApplicationController
+  def show
+    @respondent = current_user 
+  end
+
+  def edit
+    @respondent = current_user 
+  end
+
+  def update
+    @respondent = current_user
+    respondent_attrs = send("respondent_attrs")
+    if @respondent.update(respondent_attrs)
+      redirect_to action: :show
+    else
+      render action: :edit
+    end
+  end
+
+  def respondent_attrs
+    params.require(:respondent).permit(:first_name, :last_name, :phone)
+  end
+end
